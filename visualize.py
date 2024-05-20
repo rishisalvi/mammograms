1d492d952bf163d7e58ab810a4e6a9e8a1a4d880
rishisalvi <122333026+rishisalvi@users.noreply.github.com>
2024-05-20 02:33:01 -0700

    Add files via upload

---
+++ visualize.py
@@ -0,0 +1,62 @@
+#%% 
+import numpy as np 
+import pandas as pd
+from glob import glob
+import pydicom
+
+#%%
+df = pd.read_csv('mass_case_description_train_set.csv')
+#folder = 
+paths = glob('CBIS\manifest-ZkhPvrLo5216730872708713142\CBIS-DDSM\Calc-Test_P_00038_LEFT_CC' + '1-1.dcm')
+paths
+# %%
+"""import matplotlib.pyplot as plt
+import pydicom
+from pydicom.data import get_testdata_files
+filename = get_testdata_files("1-1.dcm")
+ds = pydicom.dcmread(filename)
+plt.imshow(ds.pixel_array, cmap=plt.cm.gray) """
+# %%
+from pydicom import dcmread
+ds = dcmread(r"C:\Users\rishi\Documents\MammogramData\CBIS\manifest-ZkhPvrLo5216730872708713142\CBIS-DDSM\Calc-Test_P_00038_LEFT_CC\08-29-2017-DDSM-NA-96009\1.000000-full mammogram images-63992\1-1.dcm")
+print(ds)
+plt.imshow(ds.pixel_array, cmap=plt.cm.gray)
+# %%
+from pydicom import dcmread
+#ds = dcmread(r"C:\Users\rishi\Documents\MammogramData\CBIS\manifest-ZkhPvrLo5216730872708713142\CBIS-DDSM\Calc-Test_P_00041_LEFT_CC\08-29-2017-DDSM-NA-96009\1.000000-full mammogram images-63992\1-1.dcm")
+file = r"C:\Users\rishi\Documents\MammogramData\CBIS\manifest-ZkhPvrLo5216730872708713142\CBIS-DDSM\Calc-Test_P_00041_LEFT_CC/**/**/1-1.dcm"
+paths = glob(file)
+paths
+ds1 = dcmread(paths[0])
+print(ds1)
+plt.imshow(ds1.pixel_array, cmap=plt.cm.gray) 
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+rows = 1
+cols = 2
+images = []
+images.insert(0, ds)
+images.insert(1, ds1)
+
+plt.figure(figsize=(20, 40))
+
+for i in range(rows * cols):
+    plt.subplot(rows, cols, i + 1)
+    plt.imshow(images[i].pixel_array, cmap=plt.cm.gray)  # Assuming grayscale images
+    plt.title(f'Image {i+1}', fontsize=25)
+    plt.axis('off') 
+plt.show()
+
+# %%
+df = pd.read_csv('calc_case_description_test_set.csv')
+patients = df.iloc[:, 0]
+patients = set(patients)
+patients = list(patients)
+patients
+# %%
+from pydicom import dcmread
+for i in range(len(patients)):
+    if ("P_00906" >= patients[0]):
+        file = r"C:\Users\rishi\Documents\MammogramData\CBIS\**\CBIS-DDSM\Calc-Test_" + patients[0] + "_LEFT_CC/**/**/1-1.dcm"
+# %%
